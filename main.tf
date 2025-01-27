ee31025ab2470291673e4ef22505b61e03c886e3
Fred Dixon <mrdixon1213@gmail.com>
2025-01-26 19:13:52 -0500

    updated main.tf

--- main.tf
+++ main.tf
@@ -10,4 +10,24 @@ terraform {
 
 provider "aws" {
   region = "us-east-1"
+  profile = "Terraform"
+}
+
+terraform {
+  backend "s3" {
+    bucket = "terraform-project54321"
+    key    = "terraform-project54321/backend/backend.tf"
+    region = "us-east-1"
+    encrypt = true
+    profile = "Terraform"
+  }
+}
+
+resource "aws_instance" "TerraformInstance" {
+  ami           = "ami-012967cc5a8c9f891"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "TerraformInstance"
+  }
 }
